47d8714c51030c992aea77e373e57d64888c812a
AlsoPeters <Kyle.fire@gmail.com>
2022-01-20 18:57:42 +0900

    [40] - Password Reset - Requesting a Reset

--- keystone.ts
+++ keystone.ts
@@ -1,17 +1,18 @@
-import { createAuth } from "@keystone-next/auth";
-import { User } from "./schemas/User";
-import { Product } from "./schemas/Product";
-import { ProductImage } from "./schemas/ProductImage";
-import "dotenv/config";
-import { config, createSchema } from "@keystone-next/keystone/schema";
+import { createAuth } from '@keystone-next/auth';
+import { config, createSchema } from '@keystone-next/keystone/schema';
 import {
   withItemData,
   statelessSessions,
-} from "@keystone-next/keystone/session";
-import { insertSeedData } from "./seed-data";
+} from '@keystone-next/keystone/session';
+import { User } from './schemas/User';
+import { Product } from './schemas/Product';
+import { ProductImage } from './schemas/ProductImage';
+import 'dotenv/config';
+
+import { insertSeedData } from './seed-data';
 
 const databaseURL =
-  process.env.DATABASE_URL || "mongodb://localhost/keystone-sick-fits-tutorial";
+  process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
 
 const sessionConfig = {
   maxAge: 60 * 60 * 24 * 360, // How long should they stay signed in?
@@ -19,12 +20,17 @@ const sessionConfig = {
 };
 
 const { withAuth } = createAuth({
-  listKey: "User",
-  identityField: "email",
-  secretField: "password",
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
   initFirstItem: {
-    fields: ["name", "email", "password"],
+    fields: ['name', 'email', 'password'],
     // TODO: add in initial roles here
+  },
+  passwordResetLink: {
+    async sendToken(args) {
+      console.log(args);
+    },
   },
 });
 
@@ -36,11 +42,11 @@ export default withAuth({
     },
   },
   db: {
-    adapter: "mongoose",
+    adapter: 'mongoose',
     url: databaseURL,
     async onConnect(keystone) {
-      console.log("------- Connected to the database! -------");
-      if (process.argv.includes("--seed-data")) {
+      console.log('------- Connected to the database! -------');
+      if (process.argv.includes('--seed-data')) {
         await insertSeedData(keystone);
       }
     },
@@ -53,13 +59,12 @@ export default withAuth({
   }),
   ui: {
     // Show the UI only for people who pass this test
-    isAccessAllowed: ({ session }) => {
+    isAccessAllowed: ({ session }) =>
       // console.log(session);
-      return !!session?.data;
-    },
+      !!session?.data,
   },
   session: withItemData(statelessSessions(sessionConfig), {
     // GraphQL Query
-    User: "id name email",
+    User: 'id name email',
   }),
 });
