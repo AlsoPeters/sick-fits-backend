eeceb665081511c96f9291d90c592f0939b3cabb
AlsoPeters <kyle.fire@gmail.com>
2022-05-21 12:02:51 +0900

    typo

--- keystone.ts
+++ keystone.ts
@@ -43,43 +43,42 @@ const { withAuth } = createAuth({
 
 export default withAuth(
   config({
-  server: {
-    cors: {
-      origin: [process.env.FRONTEND_URL],
-      credentials: true,
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
     },
-  },
-  db: {
-    adapter: 'mongoose',
-    url: databaseURL,
-    async onConnect(keystone) {
-      console.log('------- Connected to the database! -------');
-      if (process.argv.includes('--seed-data')) {
-        await insertSeedData(keystone);
-      }
+    db: {
+      adapter: 'mongoose',
+      url: databaseURL,
+      async onConnect(keystone) {
+        console.log('------- Connected to the database! -------');
+        if (process.argv.includes('--seed-data')) {
+          await insertSeedData(keystone);
+        }
+      },
     },
-  },
-  lists: createSchema({
-    // Schema items go in here
-    User,
-    Product,
-    ProductImage,
-    CartItem,
-    OrderItem,
-    Order,
-    Role,
-  }),
-
-  extendGraphqlSchema,
-
-  ui: {
-    // Show the UI only for people who pass this test
-    isAccessAllowed: ({ session }) =>
-      // console.log(session);
-      !!session?.data,
-  },
-  session: withItemData(statelessSessions(sessionConfig), {
-    // GraphQL Query
-    User: `id name email role { ${permissionsList.join(' ')} }`,
-  }),
-});
+    lists: createSchema({
+      // Schema items go in here
+      User,
+      Product,
+      ProductImage,
+      CartItem,
+      OrderItem,
+      Order,
+      Role,
+    }),
+    extendGraphqlSchema,
+    ui: {
+      // Show the UI only for people who pass this test
+      isAccessAllowed: ({ session }) =>
+        // console.log(session);
+        !!session?.data,
+    },
+    session: withItemData(statelessSessions(sessionConfig), {
+      // GraphQL Query
+      User: `id name email role { ${permissionsList.join(' ')} }`,
+    }),
+  })
+);
